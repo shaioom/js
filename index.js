55db528981ffa5c4bf77bdbafb847646aeb3ab01
shaima <shaima2sms@gmail.com>
2023-05-16 14:33:56 +0400

    my js

---
+++ index.js
@@ -0,0 +1,30 @@
+console.log("hello world");
+
+
+var NumberOfStudents = 25;
+console.log(NumberOfStudents)
+NumberOfStudents = 99;
+console.log(NumberOfStudents)
+NumberOfStudents = 22;
+console.log(NumberOfStudents)
+
+var x = 25;
+console.log(x)
+var y = 25;
+console.log(y)
+var z = null;
+console.log(null)
+
+var firstName = "shaima";
+var age = 27;
+console.log(firstName, "", age)
+
+console.log(firstName.indexOf("a"))
+console.log(firstName.charAt(0))
+
+var longString = "this is bery long string";
+//longString.endsWith("long");
+
+var isEndsWith = longString.endsWith("long string")
+
+console.log("IsEndsWith:", isEndsWith)
